b823d9a797173a4171bd5b4cf6feae8b177f7b59
Yamayu10 <yuki.yamamoto0711@gmail.com>
2019-11-12 21:55:23 +0900

    Add a Static Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  # 3章で追加，getリクエストHTTP(HyperText Transfer Protocol)と関連づけ
   get 'static_pages/home'
-
   get 'static_pages/help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
