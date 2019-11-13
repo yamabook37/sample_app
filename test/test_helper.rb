bda5265210b4240f52c780890ab5307b97f84d49
Yamayu10 <yuki.yamamoto0711@gmail.com>
2019-11-13 19:55:40 +0900

    Complete advanced setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 #green,red表示させたい
 require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
